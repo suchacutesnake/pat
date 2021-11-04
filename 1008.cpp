a6ea1376ebe9e60506f3fb4148b650d665fef127
sucheacutesnake <lijunting2001@gmail.com>
2021-11-05 02:33:52 +0800

    1008, simple c

---
+++ 1008.cpp
@@ -0,0 +1,18 @@
+//can also be accepted as a c program
+#include <stdio.h>
+int main() {
+    int pre = 0, N, res = 0;
+    scanf("%d", &N);
+    for (int i = 0; i < N; ++i) {
+        res += 5;
+        int cur;
+        scanf("%d", &cur);
+        if (cur > pre) res += (cur - pre) * 6;
+        else res += (pre - cur) * 4;
+        pre = cur;
+    }
+    printf("%d", res);
+    return 0;
+}
+//floor: 0     2   3     1
+// time: 5 6 6 5 6 5 4 4 0

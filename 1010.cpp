4e508f7d7f431f125349ba3e469a54d81c7f90fb
sucheacutesnake <lijunting2001@gmail.com>
2021-11-05 18:07:14 +0800

    1010, ac but confused

--- 1010.cpp
+++ 1010.cpp
@@ -1,10 +1,13 @@
 //1010 Radix (25 point(s))
+//o(1) algorithm fails at case7(Time Limit Exceeded)
+//I guess case7 is similar to: 10 9999999990 2 10
 #include <iostream>
 #include <math.h>
-//string to decimal int
-int stdi(std::string& a, int& rad) {
-    int n = a.size(), r = 0;
-    for (int i = 0; i < n; ++i) {
+using ll = long long;
+//string to decimal long long
+ll stdll(std::string& a, ll& rad) {
+    ll n = a.size(), r = 0;
+    for (ll i = 0; i < n; ++i) {
         r += (a[i] > '9' ? a[i] - 'a' + 10 : a[i] - '0') * pow(rad, (n - i - 1));
     }
     return r;
@@ -12,32 +15,36 @@ int stdi(std::string& a, int& rad) {
 
 int main() {
     std::string N1, N2, N;
-    int tag, n = 0, radix;
+    ll tag, radix;
     std::cin >> N1 >> N2 >> tag >> radix;
+    ll n = 0;
     if (tag == 1) {
-        n = stdi(N1, radix);
+        n = stdll(N1, radix);
         N = N2;
     }
     if (tag == 2) {
-        n = stdi(N2, radix);
+        n = stdll(N2, radix);
         N = N1;
     }
     char ch = '0';
     for (char& cc : N) {
         if (cc > ch) ch = cc;
     }
-    bool res = 0;
-    //left radix, current number, right radix
-    int cr = (ch > '9' ? ch - 'a' + 10 : ch - '0') + 1, cn = 0, rr = n;
-    while (cn <= n) {
-        if (cn == n) {
-            res = 1;
-            std::cout << cr - 1;
+
+    bool flag = 1;
+    ll lr = (ch > '9' ? ch - 'a' + 10 : ch - '0') + 1, rr = (n<lr?lr:n);
+    while (lr <= rr) {
+        ll midr = (lr + rr) >> 1;
+        ll mid = stdll(N, midr);
+        //printf("lr=%lld, rr=%lld, midr=%lld, mid=%lld\n", lr, rr, midr, mid);
+        if (mid < 0 || mid > n) rr = midr - 1;
+        else if (mid == n) {
+            flag = 0;
+            std::cout << midr;
             break;
         }
-        cn = stdi(N, cr);
-        ++cr;
+        else lr = midr + 1;
     }
-    if (!res) std::cout << "Impossible";
+    if (flag) std::cout << "Impossible";
     return 0;
 }

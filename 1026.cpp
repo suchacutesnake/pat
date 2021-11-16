6a1ed112c7c0c17a67eeb1c75afaa62242f716cb
sucheacutesnake <lijunting2001@gmail.com>
2021-11-16 17:07:20 +0800

    1026, 8.5hrs, disgusting description

--- 1026.cpp
+++ 1026.cpp
@@ -1,5 +1,10 @@
-//1026 Table Tennis (30 point(s))
-//The tables are numbered from 1 to N.
+/**2021.11.16 16:53 written by cutesnake
+ * 1026 Table Tennis (30 point(s))
+ * the following websites help you understand the question
+ * https://blog.csdn.net/weixin_43047175/article/details/106976403
+ * https://blog.csdn.net/qq_33231894/article/details/106948191
+ * https://blog.csdn.net/qq_39115541/article/details/99673761
+ **/
 #include <iostream>
 #include <vector>
 #include <unordered_set>
@@ -36,7 +41,7 @@ int main() {
     for (int i = 0; i < M; ++i) {
         int x;
         std::cin >> x;
-        v_idx.insert(x - 1);
+        v_idx.insert(x - 1); //indices minus by 1
     }
     //sort by arr_time
     sort(total.begin(), total.end(), cmp1);
@@ -48,16 +53,26 @@ int main() {
     for (int i = 0; i <= N; ++i) {
         //new player in queue
         q.emplace_back(i);
+        //fulfilling the condition below, it seems that this new player could play at soon
+        //but how about those people who had waited so long? Yes, we must let them play first.
         while (total[q.back()].arr_time >= min_table_time) {
             bool is_vip = 0;
+            int kk = 0x3f3f3f3f;
+            //this loop is to find if there are other tables available before the jth
             for (int m = 0; m < K; ++m) {
-                //if there are other tables available before the jth
                 if (total[q.front()].arr_time >= table[m]) {
-                    j = m;
-                    break;
+                    if (kk == 0x3f3f3f3f) kk = m; //m is the smallest available table
+                    if (!total[q.front()].tag) { //the front player in queue is not vip player
+                        break;
+                    } else { //the front player in queue is vip player
+                        if (v_idx.count(m)) { //table m is vip table
+                            kk = m; //m is the smallest available vip table
+                            break;
+                        }
+                    }
                 }
             }
-            
+            if (kk != 0x3f3f3f3f) j = kk;
             //j is a vip table
             if (v_idx.count(j)) {
                 //find vip in the queue
@@ -65,7 +80,7 @@ int main() {
                     Players& p = total[q[k]];
                     if (p.tag == 1) { //vip go to vip table
                         p.serve_time = std::max(table[j], p.arr_time);
-                        if (p.serve_time > 21 * 3600) break;
+                        if (p.serve_time >= 21 * 3600) break;
                         ++count[j];
                         p.wait_time = std::max(p.serve_time - p.arr_time, 0);
                         table[j] = p.serve_time + p.play_time;
@@ -77,7 +92,7 @@ int main() {
             }
             if (!is_vip) { //no vip in queue, queue front go to next table
                 total[q.front()].serve_time = std::max(table[j], total[q.front()].arr_time);
-                if (total[q.front()].serve_time > 21 * 3600) break;
+                if (total[q.front()].serve_time >= 21 * 3600) break;
                 ++count[j];
                 total[q.front()].wait_time = std::max(total[q.front()].serve_time - total[q.front()].arr_time, 0);
                 table[j] = total[q.front()].serve_time + total[q.front()].play_time;
@@ -87,19 +102,17 @@ int main() {
             for (int m = 0; m < K; ++m) if (table[m] == min_table_time) {j=m;break;}
             if (q.empty()) break;
         }
-        if (min_table_time > 21 * 3600) break;
+        if (min_table_time >= 21 * 3600) break;
     }
     total.pop_back(); //erase the arr_time=0x3f3f3f3f
     std::sort(total.begin(), total.end(), cmp2);
     for (Players& p : total) {
-        if (p.serve_time > 21 * 3600) break;
+        if (p.serve_time >= 21 * 3600) break;
         int h = p.arr_time / 3600, m = p.arr_time % 3600 / 60, s = p.arr_time % 60;
         int hh = p.serve_time / 3600, mm = p.serve_time % 3600 / 60, ss = p.serve_time % 60;
         printf("%02d:%02d:%02d %02d:%02d:%02d %.0lf\n", h, m, s, hh, mm, ss, round(p.wait_time / 60));//rounds up
     }
-    for (int i = 0; i < K; ++i) {
-        if (i != 0) printf(" ");
-        printf("%d", count[i]);
-    }
+    printf("%d", count[0]);
+    for (int i = 1; i < K; ++i) printf(" %d", count[i]);
     return 0;
 }

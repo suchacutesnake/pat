e8098e5e9d923e5f49a7dc48482a3744aea23b96
sucheacutesnake <lijunting2001@gmail.com>
2021-11-17 11:55:16 +0800

    1029, 20min, 47ms

--- 1029.cpp
+++ 1029.cpp
@@ -3,13 +3,12 @@
 #include <algorithm>
 #include <vector>
 int main() {
-    int a;
-    long long b;
-    std::vector<long long> v;
-    std::cin >> a;
-    for (int i = 0; i < a; ++i) {std::cin >> b; v.emplace_back(b);}
-    std::cin >> a;
-    for (int i = 0; i < a; ++i) {std::cin >> b; v.emplace_back(b);}
+    int a, b;
+    std::vector<int> v;
+    scanf("%d", &a);
+    for (int i = 0; i < a; ++i) {scanf("%d", &b); v.emplace_back(b);}
+    scanf("%d", &a);
+    for (int i = 0; i < a; ++i) {scanf("%d", &b); v.emplace_back(b);}
     int n = (v.size() - 1) >> 1;
     std::nth_element(v.begin(), v.begin() + n, v.end());
     std::cout << v[n];
